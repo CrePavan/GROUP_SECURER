5b3fab0f068fdbe19ab0c856aee59638f63efcb5
CrePavan <93823376+CrePavan@users.noreply.github.com>
2022-03-17 21:38:59 +0530

    Update __main__.py

--- EmikoRobot/__main__.py
+++ EmikoRobot/__main__.py
@@ -383,7 +383,7 @@ def emiko_about_callback(update, context):
 
 elif query.data == "codexun":
         query.message.edit_text(
-            text=f"*Welcome again 💞*"
+            text=f"*Welcome again 💞*
             I'm Group Securer Bot, the powerful group management bot which helps you to manage your super groups easily and safely. Me has a many types of unbelievable features.
 
 I also can protect your super group from scammers and spammers, for using me just add me tou your group and make a admin!
